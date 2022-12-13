e184bd531a79c112c0c21787d55164b93522b93a
yashabelkin <100130494+yashabelkin@users.noreply.github.com>
2022-12-13 15:40:06 +0200

    static files settings

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,6 @@ SIMPLE_JWT = {
 }
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -163,6 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 STATIC_URL = 'static/'
 
